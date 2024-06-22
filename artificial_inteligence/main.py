7b3f27f52db70777b769f6aa6edc62b57f910e27
ItzTas <talessabini2@gmail.com>
2024-06-21 21:51:54 -0300

    DialoGPT integrated

--- artificial_inteligence/main.py
+++ artificial_inteligence/main.py
@@ -1,10 +1,26 @@
-from transformers import pipeline # type: ignore
-from typing import List
+# Use a pipeline as a high-level helper
+from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM  # type: ignore
+from typing import Dict, List
 
-messages: List[dict[str, str]] = [
-    {"role": "user", "content": "Who are you?"},
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
+
+pipe: pipeline = pipeline("text-generation", tokenizer=tokenizer, model=model)
+
+message = Dict[str, str]
+
+messages: List[message] = [
+    {
+        "role": "user",
+        "content": "who are you?",
+    },
 ]
-pipe: pipeline = pipeline("text-generation", model="alpindale/magnum-72b-v1")
-result: str = pipe(messages)
+
+result: List[Dict[str, List[Dict[str, str]]]] = pipe(messages)
+
+
+generated_texts = result[0]["generated_text"]
 
 print(result)
+for item in generated_texts:
+    print(item["content"])
